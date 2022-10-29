39db52bf92d647d8f663f9bf002c6812d240290d
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2022-10-29 17:56:11 +0300

    Added origins to cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://dev-photodrop-client.vercel.app']
+}));
 app.use('/', usersRoutes);
 app.use('/', authRoutes);
 app.use((req, res, next) => res.status(404).send(['page not found']));
